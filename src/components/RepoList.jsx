71caee76b43a0d958bb14d32c42567227bcccc17
Andreea <andreeafarcas2803@gmail.com>
2024-07-22 09:53:38 +0300

    created components

---
+++ src/components/RepoList.jsx
@@ -0,0 +1,11 @@
+import Repo from './Repo';
+
+const RepoList = () => {
+  return (
+    <>
+      <Repo />
+    </>
+  );
+};
+
+export default RepoList;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import ProfileDetails from '../components/ProfileDetails';
+import RepoList from '../components/RepoList';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <ProfileDetails />
+      <RepoList />
+    </>
+  );
+};
+
+export default HomePage;
